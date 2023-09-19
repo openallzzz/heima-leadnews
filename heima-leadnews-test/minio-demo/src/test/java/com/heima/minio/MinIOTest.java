e85dc9e1b0bd4e32c4f1622bb5b0a1698c829548
zhangyu <1802833394@qq.com>
2023-09-19 11:02:58 +0800

    为文章提供静态页面，使用静态页面freemarker模板技术，存放至minio文件系统中

---
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/MinIOTest.java
@@ -0,0 +1,35 @@
+package com.heima.minio;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+
+import java.io.FileInputStream;
+
+public class MinIOTest {
+
+
+    public static void main(String[] args) {
+
+        FileInputStream fileInputStream = null;
+        try {
+
+            fileInputStream =  new FileInputStream("e:\\tmp\\js\\axios.min.js");;
+
+            //1.创建minio链接客户端
+            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.200.130:9000").build();
+            //2.上传
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object("plugins/js/axios.min.js")//文件名
+                    .contentType("text/js")//文件类型
+                    .bucket("leadnews")//桶名词  与minio创建的名词一致
+                    .stream(fileInputStream, fileInputStream.available(), -1) //文件流
+                    .build();
+            minioClient.putObject(putObjectArgs);
+
+            // System.out.println("http://192.168.200.130:9000/leadnews/ak47.jpg");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
