37a5d7d9e726eefbb875b0f1dcdf79b7af79e802
zhangyu <1802833394@qq.com>
2023-09-29 12:48:40 +0800

    素材列表查询功能（分页、是否收藏、时间倒序查询）

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
@@ -1,9 +1,15 @@
 package com.heima.wemedia.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.heima.file.service.FileStorageService;
+import com.heima.model.common.dtos.PageResponseResult;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.wemedia.dtos.WmMaterialDto;
 import com.heima.model.wemedia.pojos.WmMaterial;
 import com.heima.utils.thread.WmThreadLocalUtil;
 import com.heima.wemedia.mapper.WmMaterialMapper;
@@ -66,5 +72,39 @@ public class WmMaterialServiceImpl extends ServiceImpl<WmMaterialMapper, WmMater
         // 4. 返回结果
         return ResponseResult.okResult(wmMaterial);
 
+    }
+
+    /**
+     * 素材列表查询
+     *
+     * @param dto
+     * @return
+     */
+    @Override
+    public ResponseResult findList(WmMaterialDto dto) {
+        // 1. 参数
+        dto.checkParam();
+
+        // 2. 分页查询
+        IPage page = new Page(dto.getPage(), dto.getSize());
+        LambdaQueryWrapper<WmMaterial> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 是否收藏
+        if(dto.getIsCollection() != null && dto.getIsCollection() == 1) {
+            lambdaQueryWrapper.eq(WmMaterial::getIsCollection, dto.getIsCollection());
+        }
+
+        // 按照用户查询
+        lambdaQueryWrapper.eq(WmMaterial::getUserId, WmThreadLocalUtil.getUser().getId());
+
+        // 按照时间排序
+        lambdaQueryWrapper.orderByDesc(WmMaterial::getCreatedTime);
+
+        page = page(page, lambdaQueryWrapper);
+
+        // 3. 结果返回
+        ResponseResult result = new PageResponseResult(dto.getPage(), dto.getSize(), (int)page.getTotal());
+        result.setData(page.getRecords());
+
+        return result;
     }
 }
